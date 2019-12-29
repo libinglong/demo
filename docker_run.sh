cdfe201b7f472632fd37cb495bebdf451d1c41f1
libinglong <2376771274@qq.com>
2019-12-29 15:51:24 +0800

    update

--- docker_run.sh
+++ docker_run.sh
@@ -5,15 +5,15 @@ port=8080
 version=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec`
 
 mvn -U clean package -f pom.xml
-docker build -t saas/houseprice:${version} --build-arg JAR_FILE=houseprice-svc/target/houseprice.jar .
-if docker ps --format '{{.Names}}' | egrep "^saas-houseprice-${type}$" &> /dev/null; then
-    docker stop saas-houseprice-${type}
+docker build -t saas/multi-renter:${version} --build-arg JAR_FILE=multi-renter-svc/target/multi-renter.jar .
+if docker ps --format '{{.Names}}' | egrep "^saas-multi-renter-${type}$" &> /dev/null; then
+    docker stop saas-multi-renter-${type}
 else
-    echo "there is no container saas-houseprice-${type} running"
+    echo "there is no container saas-multi-renter-${type} running"
 fi
-if docker container ls -a --format '{{.Names}}' | egrep "^saas-houseprice-${type}$"; then
-    docker rm saas-houseprice-${type}
+if docker container ls -a --format '{{.Names}}' | egrep "^saas-multi-renter-${type}$"; then
+    docker rm saas-multi-renter-${type}
 else
-    echo "there is no container saas-houseprice-${type}"
+    echo "there is no container saas-multi-renter-${type}"
 fi
-docker run -d -v /opt/logs:/opt/logs/ -v /var/run/docker.sock:/var/run/docker.sock -p ${port}:8080 -p 5005:5005  --restart=always --name=saas-houseprice-${type} saas/houseprice:${version}
+docker run -d -v /opt/logs:/opt/logs/ -v /var/run/docker.sock:/var/run/docker.sock -p ${port}:8080 -p 5005:5005  --restart=always --name=saas-multi-renter-${type} saas/multi-renter:${version}
