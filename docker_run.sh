55502c9fffa4516d4e99f0809cf7769f916da287
libinglong <2376771274@qq.com>
2019-12-29 17:43:05 +0800

    up

--- docker_run.sh
+++ docker_run.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-echo $USER
 type=snapshot
 port=8080
 version=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.6.0:exec`
 
 mvn -U clean package -f pom.xml -Dmaven.test.skip=true
-pwd
-docker build -t saas/multi-renter:${version} --build-arg JAR_FILE=multi-renter/target/multi-renter.jar .
+docker build -t saas/multi-renter:${version} --build-arg JAR_FILE=target/multi-renter.jar .
 if docker ps --format '{{.Names}}' | egrep "^saas-multi-renter-${type}$" &> /dev/null; then
     docker stop saas-multi-renter-${type}
 else
